19cf2848f3430edbd7f74bfe61a8edb01859a69f
mbaglierii <46365798@ifts24.edu.ar>
2024-09-30 17:19:48 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/ofertas', (req, res) => {
-    res.sendFile(__dirname + "/public/" + "ofertas.html");
+    res.sendFile(__dirname + "ofertas.html");
 });
 
 app.listen(PORT, () => {
